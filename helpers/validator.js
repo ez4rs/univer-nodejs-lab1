223371b4b53081eece2951e30bff35d2c462ee51
ez4rs <govsha.ars@gmail.com>
2021-12-20 12:19:01 +0300

    added validation

---
+++ helpers/validator.js
@@ -0,0 +1,29 @@
+class Validator{
+    static isEmpty(value) {
+        if (value === undefined) return true;
+
+        if (
+            typeof value == "function" ||
+            typeof value == "number" ||
+            typeof value == "boolean" ||
+            Object.prototype.toString.call.value === "[object Date]"
+        ) return false;
+
+        return value === null || value.length === 0;
+
+
+    }
+
+    static isIn(value, array) {
+        return array.includes(value);
+    }
+
+    static isArray(value) {
+        if (Array.isArray(value))
+            return value;
+        else {
+            process.stderr.write("Value is not an array.\n");
+            process.exit(1);
+        }
+    }
+}
